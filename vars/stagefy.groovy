0181dfa91c7bc4e27be92753aff8fddb9272fd16
BENJAMIN JIN <60589873+jinrudals@users.noreply.github.com>
2023-07-11 14:47:18 +0900

    Update stagefy.groovy

--- vars/stagefy.groovy
+++ vars/stagefy.groovy
@@ -10,19 +10,24 @@ import groovy.transform.InheritConstructors
 import groovy.json.JsonSlurper
 
 class Stagefy {
-  String filename
-  String stagename
-  Object script
-  def parent
+  String filename           // file of stage
+  String stagename          // name of stage
+  Object script             // wrapper to use global jenkins functions
+  boolean flag              // flag of stage. If true, execute the stage, else do not not single stage
+  def parent                // parent of current stage
   Stagefy(filename, stagename, parent, script){
     this.filename = filename
     this.stagename = stagename
     this.script = script
+    this.flag   = true
     this.parent = parent
   }
+  // load stage data from file
   def load_data(String filename, String stagename){
     return this.script.load_data(filename, stagename)
   }
+
+  // single stage run wrapper
   public def steps_run(){
     def data = load_data(this.filename, this.stagename)
     def pattern = /\$\{\s*env\.[a-z|A-Z|_|\.]*\s*\}*/
@@ -36,7 +41,6 @@ class Stagefy {
     }
     def content = {
       // If parent is parallel, the stage is created
-      // if(this.parent != null && this.parent.tpe == "parallels") {
         for(each in data["steps"]){
           if(each.containsKey("sh")){
             def s_shell = each['sh']
@@ -52,9 +56,11 @@ class Stagefy {
             this.script.sh(finaloutput)
           }else if(each.containsKey("script")){
             def scriptFile = each["script"]
-            this.sript.load(scriptFile).main()
+            this.script.load(scriptFile).main()
           }else if(each.containsKey("setEnvFromFile")){
             this.script.setEnvFromFile(each["setEnvFromFile"])
+          }else if(each.containsKey("evaluate")){
+            this.script.evaluation(each["evaluate"])
           }
         }
     }
@@ -83,10 +89,11 @@ class Stagefy {
         }
       })
     }
-
-    contents[-1]()
+    if(this.flag){
+      contents[-1]()
+    }
   }
-
+  // parallel stage run wrapper
   public def parallels_run(){
     def temp = load_data(this.filename, this.stagename)['parallels']
     def data = [:]
@@ -114,11 +121,10 @@ class Stagefy {
     this.script.parallel data
   }
 
+  // sequentail stage run wrapper
   public def stages_run(script) {
 
-    println("DEBUGGING :::: ")
     def temp = this.script.load_data(this.filename, this.stagename)['stages']
-    println(temp)
     def inside = []
     for(each in temp){
       def nextFile = "\$HERE"
@@ -134,26 +140,33 @@ class Stagefy {
       inside.add(this.construct_stage(nextFile, nextStage))
     }
 
-    println(inside)
-    this.script.sh("echo ${this.stagename} from ${this.filename}")
+
       for(each in inside){
         this.script.stage(each.stagename){
           each.run()
         }
       }
   }
+
+  // construct child stage instance
   def construct_stage(String filename, String stagename) {
     return this.getClass().newInstance(filename, stagename, this, this.script)
   }
 
+  // check circluar loop
   def check_circular_loop(other){
+    // if no parent, it is passed
     if (this.parent == null){
       return true
     }else {
-
+      /*
+        When the parent and other are from the same file and have same stage name,
+        it is circular loop
+      */
       if(this.parent.filename == other.filename && this.parent.stagename == other.stagename){
         throw new Exception("Circular Loop Execution ${this.stagename} from ${other.filename}")
       }else{
+        // Check with current parent tree
         this.parent.check_circular_loop(other)
       }
     }
@@ -161,47 +174,42 @@ class Stagefy {
   public def run(){
     def temp = this.load_data(this.filename, this.stagename)
     def whenData = temp["when"]
-    this.script.echo("DEBUG POINT #1")
-    this.script.echo("Before data :: ${whenData}")
+    // If when data is empty, it is true by default
     if(whenData == null){
       whenData = true
     } else {
+      // otherwise, evaluate the string value
       whenData = this.script.evaluation(whenData)
     }
-    this.script.echo("After data :: ${whenData}")
-    if(whenData){
-      if(temp.stages != null){
-        this.check_circular_loop(this)
-        return this.stages_run()
-      }
-      else if(temp.parallels != null){
-        this.check_circular_loop(this)
-        return this.parallels_run()
-      }
-      else if(temp.steps != null){
-        return this.steps_run()
-      }
-      else {
-        this.script.error('No matching type')
-      }
-    }else [
-      Utils.markStageSkippedForConditional(this.stagename)
-    ]
-  }
-}
-
-def run_parallels(name, ss){
-  return {
-    stage(name) {
-      try {
-        parallel ss
-      } catch(e) {
-        throw e
-      }
+    this.flag = whenData
+    // If parent exists, check the parent skip condition
+    if(this.parent != null){
+      this.flag = this.parent.flag && this.flag
     }
+    // If skipped condition, mark jenkins stage as skipped
+    if(!this.flag){
+      Utils.markStageSkippedForConditional(this.stagename)
+    }
+    // Run each type
+    if(temp.stages != null){
+      this.check_circular_loop(this)
+      return this.stages_run()
+    }
+    else if(temp.parallels != null){
+      this.check_circular_loop(this)
+      return this.parallels_run()
+    }
+    else if(temp.steps != null){
+      return this.steps_run()
+    }
+    else {
+      this.script.error('No matching type')
+    }
+
   }
 }
 
+// wrapper for evaluation
 def evaluation(value){
   return evaluate(value)
 }
